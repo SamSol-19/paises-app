39460c2bf70e6239fbca5b22f48dd5f27db6fc5a
SamSol-19 <samanthalindoboza@gmail.com>
2025-01-07 01:24:34 -0500

    primera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
+import { AboutPageComponent } from "./shared/pages/about-page/about-page.component";
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {
+        path: 'home',
+        component: HomePageComponent
+    },
+    {
+        path: 'about',
+        component: AboutPageComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
