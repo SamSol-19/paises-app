833e5d6f721f52a7760bbe206a5314174f24fb97
SamSol-19 <samanthalindoboza@gmail.com>
2025-01-07 01:37:44 -0500

    Actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
 import { AboutPageComponent } from "./shared/pages/about-page/about-page.component";
+import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
