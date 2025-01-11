fa85d2c13f3e494e9ff3c21f93a09d84fc9966c5
SamSol-19 <samanthalindoboza@gmail.com>
2025-01-10 22:33:30 -0500

    Personalizado

--- src/app/paises/components/pais-table/pais-table.component.ts
+++ src/app/paises/components/pais-table/pais-table.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Pais } from '../../interfaces/pais';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'paises-table',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './pais-table.component.html',
   styleUrl: './pais-table.component.css'
 })
